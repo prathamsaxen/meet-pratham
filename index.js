76130f0b4d400b8ceed9d01653799357289f7750
Pratham Saxena <94908262+prathamsaxen@users.noreply.github.com>
2025-03-05 23:18:38 +0530

    Implement Console.log for Pratham Terminal Intro

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+console.log(`
+    +-------------------------------- Meet Pratham Saxena 👋🏻 --------------------------------+
+          Hey there! I'm Pratham, a passionate Software Engineer 🚀 & AI Enthusiast 🤖      
+    ----------------------------------------------------------------------------------------
+         📍 GitHub:    https://github.com/prathamsaxen                                                                        
+         📍 LinkedIn:  https://prathamsaxena.vercel.app/                            
+         📍 Portfolio: https://prathamsaxena.dev/                                                                                   
+         📍 Email:     er.prathamsaxena@gmail.com                             
+    ----------------------------------------------------------------------------------------
+                              💡 Tech Stack & Expertise                                      
+    ----------------------------------------------------------------------------------------
+         ✅  React.js & Next.js        ✅  MERN Stack Development  
+         ✅  UI/UX Optimization        ✅  API Design & Integration  
+         ✅  Agile & Jira Workflow     ✅  DevOps & Cloud (AWS)  
+         ✅  Performance Optimization  ✅  Technical Mentorship                                                    
+    ----------------------------------------------------------------------------------------
+                              🚀 Work Experience                                              
+    ----------------------------------------------------------------------------------------
+         🔹 Truxt AI (Software Engineer) ➝ Building AI-driven SaaS solutions with MERN stack.  
+         🔹 Codefeast (Mentor) ➝ Trained 100+ students in React, Redux, and modern web dev.  
+         🔹 GeeksforGeeks (Technical Content Engineer) ➝ Reviewed 1000+ dev articles, built tools.  
+    ----------------------------------------------------------------------------------------
+                              🎯 Featured Projects                                            
+    ----------------------------------------------------------------------------------------
+         ⚡  TripSync (AI-Powered Travel Assistant) ➝ https://tripsync.com/  
+         ⚡  Truxt AI (Enterprise AI Chat & Data Solutions) ➝ https://truxt.ai/  
+         ⚡  React-UI Components (Reusable UI library) ➝ https://npmjs.com/package/react-ui-component  
+    +----------------------------------------------------------------------------------------+
+                                🚀 Let's Build Something Awesome!                              
+    ----------------------------------------------------------------------------------------
+    `);
+    
